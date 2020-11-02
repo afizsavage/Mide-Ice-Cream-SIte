2033fba4c5fb9ff0b8605527ce86e1cb8cab6696
Afiz Savage <savageafiz9@gmail.com>
2020-11-02 09:21:31 +0000

    contact us form added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
-import { ListGroup } from "react-bootstrap"
+import { ListGroup, Form, Row, Col } from "react-bootstrap"
 import { ImYoutube, ImFacebook, ImInstagram, ImTwitter } from "react-icons/im"
 
 const Footer = () => {
   return (
     <footer className="d-flex flex-column">
-      <div className="topFooterContent pb-4 ">
+      <div className="d-flex flex-column topFooterContent pb-4">
+        <Row className="row connect">
+          <Col sm className="contact justify-content-center">
+            {" "}
+            <div className="inner-contact col-md-8 offset-lg-6 col-lg-5">
+              <Form>
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                  <h3>Connect with us</h3>
+                  <Form.Label>How can we help you?</Form.Label>
+                  <Form.Control as="select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form>
+            </div>
+          </Col>
+          <Col sm className=" newsletter">
+            {" "}
+            <div className="inner-newsletter">
+              <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+              </Form>
+            </div>
+          </Col>
+        </Row>
         <div className="socialIcons pb-4 pt-2 my-1">
           <Link className="icon rounded-circle" to="utube.com">
             <ImYoutube size={20} />
